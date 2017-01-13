db418b6e7fb07c7b40268d651c4a44f3f463cf75
Zeebroek <woutvt@telenet.be>
2017-01-13 11:38:17 +0000

    Fixed symlink script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
-##########################
+############################
 # .make.sh
-# This script creates symlinks from the home directory to any desired dotfile in ~/dotfiles
-##########################
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
 
-#### Variables
+########## Variables
 
-dir=~/dotfiles		# dotfiles directory
-olddir=~/dotfiles_old	# old dotfiles backup directory
-files="Xresources"	# list of files/folders to symlink to homedir
+dir=~/dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+files="Xresources"    # list of files/folders to symlink in homedir
 
-####
+##########
 
-# Create dotfiles_old in homedir
+# create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
 echo "...done"
 
-# Change to the dotfiles directory
+# change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
-# Move any existing dotfile in homedir to dotfiles_old directory, then create symlinks
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-	echo "Moving any existing dotfile from ~ to $olddir"
-	mv ~/.$file ~/dotfiles_old/
-	echo "Creating symlink to $file in home directory"
-	ln -s $dir/$file ~/.$file
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
 done
